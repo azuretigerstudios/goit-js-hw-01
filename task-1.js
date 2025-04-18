a0d81b68ffb5c869890125832c0d3b888de6d116
azuretigerstudios <azuretigerstudios@gmail.com>
2025-04-19 01:42:36 +0300

    update index and task1

--- task-1.js
+++ task-1.js
@@ -4,5 +4,6 @@ function makeTransaction(quantity, pricePerDroid) {
 }
 
 // Test örnekleri:
-console.log(makeTransaction(3, 800));   // "You ordered 3 droids worth 2400 credits!"
-console.log(makeTransaction(5, 1200));  // "You ordered 5 droids worth 6000 credits!"
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
